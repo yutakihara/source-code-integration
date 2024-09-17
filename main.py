a6dea377eb0f65fcc56768f3658b9d085f9cf97b
yutakihara <yuta.kihara@datadoghq.com>
2024-09-17 14:53:17 +0900

    delete wrapper

--- main.py
+++ main.py
@@ -40,4 +40,5 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    log.info('End of Main Function HERE!!!!!!!!!!!!!!!!!!!!!!!!')
 # [END cloudrun_helloworld_service]
